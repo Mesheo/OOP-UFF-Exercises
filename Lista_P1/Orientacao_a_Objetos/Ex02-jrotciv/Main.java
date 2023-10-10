b6f0378b8ddd6ae39a5d4e1ddded4fb7bb504dcc
João Victor Souza <jvsouzasantos@id.uff.br>
2023-10-10 16:38:31 -0300

    feat: add metodos de visualizacao e exibindo as informaçoes

--- Lista_P1/Orientacao_a_Objetos/Ex02-jrotciv/Main.java
+++ Lista_P1/Orientacao_a_Objetos/Ex02-jrotciv/Main.java
@@ -6,53 +6,74 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Main {
+    //Total de emprestimos da biblioteca
+    public int quantidadeLivrosEmprestados(List<Emprestimo> emprestimos){
+        return emprestimos.size();
+    }
+
+    //Quantos livros disponiveis
+    public int totalDisponiveis(List<Livro> livros,  List<Emprestimo> emprestimos){
+        return livros.size() - emprestimos.size();
+    }
+
+    //Saber quantos emprestimos uma pessoa tem ativos
+    public int totalEmprestadosParaPessoa(String codPessoa, List<Emprestimo> emprestimos){
+        int total = 0;
+
+        for (Emprestimo emprestimo : emprestimos){
+            if (emprestimo.getCliente() == codPessoa){
+                total++;
+            }
+        }
+
+        return total;
+    }
+
     public static void main(String[] args){
         List<Pessoa> pessoas =  new ArrayList<>();
         List<Livro> livros =  new ArrayList<>();
         List<Emprestimo> emprestimos =  new ArrayList<>();
 
+        Main main = new Main();
         Pessoa pessoa1 = new Pessoa("Joao", "12310845");
-        Livro livro1 = new Livro("Percy Jackson", "Rick Riordan", "12154510");
-        Emprestimo emprestimo1 = new Emprestimo(pessoa1, livro1, 0);
-
+        Pessoa pessoa2 = new Pessoa("Victor", "54643167");
         pessoas.add(pessoa1);
-        livros.add(livro1);
-        emprestimos.add(emprestimo1);
+        pessoas.add(pessoa2);
 
+        Livro livro1 = new Livro("Percy Jackson", "Rick Riordan", "72154510");
+        Livro livro2 = new Livro("O nome do vento", "Patrick Rothfuss", "84881484");
+        livros.add(livro1);
+        livros.add(livro2);
+
+        Emprestimo emprestimo1 = new Emprestimo(pessoa1, livro1, 0);
+        emprestimos.add(emprestimo1);
 
         String linha = "-".repeat(50);
         
         System.out.println("Clientes:");
         for (Pessoa pessoa : pessoas){
             System.out.println(linha);
-            System.out.println("Nome: " + pessoa.getNome());
-            System.out.println("Codigo: " + pessoa.getCodCliente());
+            pessoa.exibe();
+            System.out.println("Total de livros em posse: " + main.totalEmprestadosParaPessoa(pessoa.getCodCliente(), emprestimos));
             System.out.println(linha);
         }
 
-        System.out.println("Livros:");
+        System.out.println("Livros da biblioteca:");
         for (Livro livro : livros){
             System.out.println(linha);
-            System.out.println("Titulo: " + livro.getTitulo());
-            System.out.println("Autor: " + livro.getAutor());
-            System.out.println("Codigo: " + livro.getCod());
-            System.out.println("Disponivel: " + livro.isDisponivel());
+            livro.exibe();
             System.out.println(linha);
         }
 
-        System.out.println("Emprestimos:");
+        System.out.println("Emprestimos da biblioteca:");
         for (Emprestimo emprestimo : emprestimos){
             System.out.println(linha);
-            System.out.println("Codigo do livro: " + emprestimo.getLivro());
-            System.out.println("Codigo do cliente: " + emprestimo.getCliente());
-            System.out.println("Data do emprestimo: " + emprestimo.getDataEmprestimo());
-            System.out.println("Data de devolucao: " + emprestimo.getDataDevolucao());
+            emprestimo.exibe();
             System.out.println(linha);
         }
 
-    }
+        System.out.println("Total de livros que estao emprestados: " + main.quantidadeLivrosEmprestados(emprestimos));
+        System.out.println("Total de livros disponiveis na biblioteca: " + main.totalDisponiveis(livros, emprestimos));
 
-    //Saber quantos emprestimos uma pessoa tem ativos
-    //Total de emprestimos da biblioteca
-    //Quantos livros livres
+    }
 }
